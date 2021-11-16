917248cb9d5f22a7fdbf53a13a2716b4a607ae25
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-11-16 13:19:47 -0500

    debugged filtering with webGPU

--- src/drawLines.ts
+++ src/drawLines.ts
@@ -1,5 +1,6 @@
 var filtersAdded = false;
-var filters = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var filters = [15,1,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,82.4, 2.24, 38.758, 53.1, 0.5667, 4.74, 89, 74.46, 100, 98.69, 902, 95.6, 83.689, 74.623, -1];
+
 var names = [
   'protein (g)', 'calcium (g)',
   'sodium (g)', 'fiber (g)',
@@ -8,7 +9,7 @@ var names = [
   'fat (g)', 'water (g)',
   'calories', 'saturated (g)',
   'monounsat (g)', 'polyunsat (g)']
-var max: Array<number> = [
+var max = [
   82.4, 2.24, 38.758, 53.1, 0.5667, 4.74, 89, 74.46, 100, 98.69, 902, 95.6, 83.689, 74.623];
 var data;
 
@@ -43,7 +44,7 @@ function createFilter(name: string, max: number, index: number) {
   filter.value = "0";
   filter.max = max.toString();
   filter.onchange = function (e) {
-    filters[index] = Number.parseFloat(filter.value);
+    filters[index + 2] = Number.parseFloat(filter.value);
     drawX();
   }
 
@@ -191,9 +192,185 @@ function getPipline(device: GPUDevice, gpu: any) {
   return pipeline;
 }
 
-let CreateLineStrips = async (lineStrips: Float32Array) => {
+let CreateLineStrips = async (data: Float32Array, indices: Float32Array) => {
   let gpu = await InitGPU();
   let device = gpu.device;
+
+const gpuBufferdataMatrix = device.createBuffer({
+  mappedAtCreation: true,
+  size: data.byteLength,
+  usage: GPUBufferUsage.STORAGE
+});
+
+const arrayBufferdataMatrix = gpuBufferdataMatrix.getMappedRange();
+new Float32Array(arrayBufferdataMatrix).set(data);
+gpuBufferdataMatrix.unmap();
+
+// Filter Buffer
+
+console.log("num rows", data[0]);
+console.log("num rows", ( data[0] - 2) / filters[0]);
+const filterArray = new Float32Array(filters)
+const gpuBufferFilterMatrix = device.createBuffer({
+  mappedAtCreation: true,
+  size: filterArray.byteLength,
+  usage: GPUBufferUsage.STORAGE
+});
+
+const arrayBufferFilterMatrix = gpuBufferFilterMatrix.getMappedRange();
+new Float32Array(arrayBufferFilterMatrix).set(filterArray);
+gpuBufferFilterMatrix.unmap();
+
+const maxArray = new Float32Array(max)
+const gpuBufferMaxMatrix = device.createBuffer({
+  mappedAtCreation: true,
+  size: maxArray.byteLength,
+  usage: GPUBufferUsage.STORAGE
+});
+
+const arrayBufferMaxMatrix = gpuBufferMaxMatrix.getMappedRange();
+new Float32Array(arrayBufferMaxMatrix).set(maxArray);
+gpuBufferFilterMatrix.unmap();
+// Result Matrix
+
+const resultMatrixBufferSize = Float32Array.BYTES_PER_ELEMENT * (2 + ( 2 * data[0] * data[1]));
+const resultMatrixBuffer = device.createBuffer({
+  size: resultMatrixBufferSize,
+  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC
+});
+
+
+
+const shaderModule = device.createShaderModule({
+  code: `
+  [[block]] struct Matrix {
+    size : vec2<f32>;
+    numbers: array<f32>;
+  };
+
+  [[group(0), binding(0)]] var<storage, read> dataMatrix : Matrix;
+  [[group(0), binding(1)]] var<storage, read> filterMatrix : Matrix;
+  [[group(0), binding(2)]] var<storage, write> resultMatrix : Matrix;
+  [[group(0), binding(3)]] var<storage, write> resultMatrix2 : Matrix;
+ 
+  
+  [[stage(compute), workgroup_size(8, 8)]]
+  fn main([[builtin(global_invocation_id)]] global_id : vec3<u32>) {
+    // Guard against out-of-bounds work group sizes
+    if (global_id.x >= u32(dataMatrix.size.x) || global_id.y >= u32(filterMatrix.size.y)) {
+      return;
+    }
+
+    resultMatrix.size = vec2<f32>(dataMatrix.size.x * dataMatrix.size.y, f32(2.0));
+    var row = 0.0;
+    var dataIndex = 0;
+    
+
+    // Goes through all rows in the dataMatrix
+    for (var i = 0u; i < u32(dataMatrix.size.x); i = i + 1u)
+    {
+      var valid = 0;
+
+      for (var k = 0u; k < u32(filterMatrix.size.x - 1.0); k = k + 1u)
+      {
+        if (dataMatrix.numbers[dataIndex] < ( filterMatrix.numbers[k] / filterMatrix.numbers[u32(filterMatrix.size.x) + k]))
+        {
+          valid = 1;
+        }
+        dataIndex = dataIndex + 1;
+      }
+
+      dataIndex = dataIndex - i32(filterMatrix.size.x - 1.0);
+      
+      
+        for (var j =0u; j < u32(filterMatrix.size.x); j = j + 1u)
+        {
+         
+          if (valid == 0)
+          {
+            resultMatrix.numbers[dataIndex] = dataMatrix.numbers[dataIndex];
+           
+          }
+          else
+          {
+            resultMatrix.numbers[dataIndex] = 0.0;
+          }
+          dataIndex = dataIndex + 1;
+         
+        }      
+    }
+  }
+  `
+});
+  // Pipeline setup
+  
+  const computePipeline = device.createComputePipeline({
+    compute: {
+      module: shaderModule,
+      entryPoint: "main"
+    }
+  });
+
+const bindGroup = device.createBindGroup({
+  layout: computePipeline.getBindGroupLayout(0 /* index */),
+  entries: [
+    {
+      binding: 0,
+      resource: {
+        buffer: gpuBufferdataMatrix
+      }
+    },
+    {
+      binding: 1,
+      resource: {
+        buffer: gpuBufferFilterMatrix
+      }
+    },
+    {
+      binding: 2,
+      resource: {
+        buffer: resultMatrixBuffer
+      }
+    },
+    
+  ]
+});
+
+
+
+var commandEncoder1 = device.createCommandEncoder();
+
+const passEncoder = commandEncoder1.beginComputePass();
+passEncoder.setPipeline(computePipeline);
+passEncoder.setBindGroup(0, bindGroup);
+passEncoder.dispatch(8, 8);
+passEncoder.endPass();
+
+
+// Get a GPU buffer for reading in an unmapped state.
+const gpuReadBuffer = device.createBuffer({
+  size: resultMatrixBufferSize,
+  usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
+});
+
+// Encode commands for copying buffer to buffer.
+commandEncoder1.copyBufferToBuffer(
+  resultMatrixBuffer /* source buffer */,
+  0 /* source offset */,
+  gpuReadBuffer /* destination buffer */,
+  0 /* destination offset */,
+  resultMatrixBufferSize /* size */
+);
+
+// Submit GPU commands.
+const gpuCommands = commandEncoder1.finish();
+device.queue.submit([gpuCommands]);
+
+// Read buffer.
+await gpuReadBuffer.mapAsync(GPUMapMode.READ);
+const arrayBuffer = gpuReadBuffer.getMappedRange();
+console.log(new Float32Array(arrayBuffer));
+
 
   let colorData = new Float32Array([.5, .5, .5]);
 
@@ -213,13 +390,14 @@ let CreateLineStrips = async (lineStrips: Float32Array) => {
 
   let pipeline = getPipline(device, gpu);
 
-  let indirectParameters = getIndirectParameters(lineStrips.length / 2);
+  let indirectParameters = getIndirectParameters(data.length / 2);
   let indrectBuffer = CreateGPUBufferIndirect(device, indirectParameters);
-  let vertexBuffer = CreateGPUBufferFloat32(device, lineStrips);
+  let vertexBuffer = CreateGPUBufferFloat32(device, new Float32Array(arrayBuffer));
   let colorBuffer = CreateGPUBufferFloat32(device, colorData);
 
   renderPass.setPipeline(pipeline);
   renderPass.setVertexBuffer(0, vertexBuffer);
+  renderPass.setVertexBuffer(2, vertexBuffer);
   renderPass.setVertexBuffer(1, colorBuffer);
   renderPass.drawIndirect(indrectBuffer, 0);
   renderPass.endPass();
@@ -233,6 +411,41 @@ function isValidLine() {
   return true;
 }
 
+
+function prepareData(data: Array<JSON>)
+{
+  var categories = Object.values(data[0]);
+  categories = categories.slice(2, categories.length);
+
+  // Array starts with #rows and #columns
+  var yValues = []
+  var xValues = []
+  xValues.push(data.length * categories.length + 1);
+  xValues.push(1);
+  yValues.push(data.length);
+  yValues.push(categories.length + 1);
+
+  for (var i = 0; i < data.length; i++)
+  {
+    var row = Object.values(data[i])
+    row = row.slice(2, row.length);
+
+    for (var j = 0; j < row.length; j++)
+    {
+      var index = ((- row.length / 2) + j ) / row.length;
+      xValues.push(index * 2.5);
+      yValues.push((row[j] / max[j]) * 7.5);
+      if (j + 1 === row.length)
+      {
+        xValues.push(0.0);
+        yValues.push(0.0);
+      }
+    }
+  }
+  return {yValues: yValues,
+          xValues: xValues
+  };
+}
 function cleanData(data: Array<JSON>) {
 
   // Help scaling for making it full width need to figure out what exact values are the best
@@ -251,7 +464,6 @@ function cleanData(data: Array<JSON>) {
       if (row[j] !== null && row[j] !== undefined) {
         max[j] = Math.max(row[j], max[j]);
       }
-
     }
   }
 
@@ -298,9 +510,8 @@ export function drawX() {
     var w = 1;
     var h = 1;
     var data = this.response;
-    data = cleanData(data)
-    var results = new Float32Array(data);
-    CreateLineStrips(results);
+    var buffer = prepareData(data);
+    CreateLineStrips(new Float32Array(buffer.yValues),new Float32Array(buffer.xValues));
   };
   request.send();
 }
